de7bea6144032e163947058d0baa4737cb0c797c
jscmns <camunasluis@gmail.com>
2022-07-19 16:05:05 -0300

    Basic endpoint creation and parsing w/out Express

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 const http = require('http');
 const util = require('util');
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  console.log(`URL: ${req.url} \n METHOD: ${req.method}, \n HEADERS: ${util.inspect(req.headers)}`);
+  const url = req.url;
+  const method = req.method.toUpperCase();
+
+  if (url === '/') {
+    res.write(` 
+    <body>
+      <h1>Enter message</h1>
+      <form action="/message" method="POST">
+        <input type="text" name="message" label="message"/>
+        <button type="submit">Submit</button>
+      </form>
+    </body>
+    `);
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => { body.push(chunk); });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
+  }
+
   res.setHeader('Content-Type', 'text/html');
-  res.write('<h1>Hello World buddy</h1>');
-  // Response must not change after ending it.
+  res.write(`<h1>Hello from Node.js</h1>`);
   res.end();
 });
 
