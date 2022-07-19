bfbd8d4e9f41ca57497b896eced20a579185bcb4
jscmns <camunasluis@gmail.com>
2022-07-19 17:41:41 -0300

    complete assignment 1

--- routes.js
+++ routes.js
@@ -4,20 +4,39 @@ const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
 
+  res.setHeader('Content-Type', 'text/html');
+
   if (url === '/') {
     res.write(` 
-  <body>
-    <h1>Enter message</h1>
-    <form action="/message" method="POST">
-      <input type="text" name="message" label="message"/>
-      <button type="submit">Submit</button>
-    </form>
-  </body>
-  `);
+      <body>
+        <h1>Welcome to fake website</h1>
+        <form method="POST" action="/create-user">
+          <input type="text" name="username" />
+          <button type="submit">Add user</button>
+        </form>
+      </body>
+    `);
     return res.end();
   }
 
-  if (url === '/message' && method === 'POST') {
+  if (url === '/users') {
+    res.write(` 
+      <body>
+        <h1>Fake user list</h1>
+        <ul>
+          <li>User 1</li>
+          <li>User 2</li>
+          <li>User 3</li>
+          <li>User 4</li>
+          <li>User 5</li>
+          <li>User 6</li>
+        </ul>
+      </body>
+    `);
+    return res.end();
+  }
+
+  if (url === '/create-user' && method === 'POST') {
     const body = [];
     // req methods are asynchronous
     req.on('data', chunk => {
@@ -25,18 +44,13 @@ const requestHandler = (req, res) => {
     });
     return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, err => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
+      const user = parsedBody.split('=')[1];
+      console.log(user);
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      return res.end();
     });
   }
-
-  res.setHeader('Content-Type', 'text/html');
-  res.write(`<h1>Hello from Node.js</h1>`);
-  res.end();
 };
 
 module.exports = requestHandler;
