9907e89517e41ab56eb324a45fadbe52ebe36c22
jscmns <camunasluis@gmail.com>
2022-07-25 15:36:13 -0300

    pug-handlebars test

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const http = require('http');
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+const { engine } = require('express-handlebars');
 const app = express();
 
-app.use('/users', (request, response, next) => {
-  console.log('Users list');
-});
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use((request, response, next) => {
-  console.log('In root route')
-  response.send("We are in root route");
+app.engine('hbs', engine({
+  extname: "hbs",
+  defaultLayout: "main-layout",
+  layoutsDir: "views/layouts",
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page not found' });
 });
 
 app.listen(3000);
