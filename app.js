0ee79c8b3f9ef8367679046532e9f02571fb842b
jscmns <camunasluis@gmail.com>
2022-07-26 13:20:01 -0300

    ejs

--- app.js
+++ app.js
@@ -2,18 +2,12 @@ const http = require('http');
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const { engine } = require('express-handlebars');
 const app = express();
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.engine('hbs', engine({
-  extname: "hbs",
-  defaultLayout: "main-layout",
-  layoutsDir: "views/layouts",
-}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +17,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page not found' });
+  res.status(404).render('404', { pageTitle: 'Page not found', path: false });
 });
 
 app.listen(3000);
